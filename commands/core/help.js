e107c3d24fb84ee3734bf1f21ee144f28b2fee17
valerylusty-glitch <valerylusty@gmail.com>
2026-04-06 19:25:04 -0500

    Create help command

---
+++ commands/core/help.js
@@ -0,0 +1,8 @@
+// Help command that lists all commands by category
+module.exports = {
+    name: 'help',
+    description: 'Lists all commands by category',
+    execute(message) {
+        // Logic to list commands
+    }
+};
