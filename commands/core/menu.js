922eee27426ec92571a56bbf55604dabc8fb971a
valerylusty-glitch <valerylusty@gmail.com>
2026-04-06 19:27:52 -0500

    Create menu.js file for main menu of LUST Bot

---
+++ commands/core/menu.js
@@ -0,0 +1,7 @@
+// Main Menu for LUST Bot
+
+const mainMenu = () => {
+    return `Welcome to LUST Bot!\n\nAvailable command categories:\n1. General Commands\n2. Music Commands\n3. NSFW Commands\n4. Fun Commands\n5. Utility Commands\n6. Admin Commands\n\nUse the respective numbers to access commands in each category!`;
+};
+
+module.exports = mainMenu;
